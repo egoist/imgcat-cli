e2ff4fd6fc2dcf7b50db17651dc36ba9c9307fb5
EGOIST <0x142857@gmail.com>
2016-03-16 22:55:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# imgcat-cli [![NPM version](https://img.shields.io/npm/v/imgcat-cli.svg)](https://npmjs.com/package/imgcat-cli) [![NPM downloads](https://img.shields.io/npm/dm/imgcat-cli.svg)](https://npmjs.com/package/imgcat-cli)
+
+![preview](https://ooo.0o0.ooo/2016/03/16/56e9739f6a18a.gif)
+
+## Install
+
+```bash
+$ npm install -g imgcat-cli
+```
+
+## Usage
+
+```js
+$ imgcat http://...
+
+$ imgcat ./path/to/a.gif
+```
+
+All [imgcat](https://github.com/egoist/imgcat) options can be used here via CLI flags, like `--width 40`
+
+## License
+
+MIT © [EGOIST](https://github.com/egoist)
---
+++ cli.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict'
+const imgcat = require('imgcat')
+const meow = require('meow')
+const Spin = require('io-spin')
+
+const spin = new Spin('Box1')
+
+const cli = meow(`
+  Usage:
+
+    imgcat <path|url>
+
+    -w/--width    Image width
+    -h/--height   Image height
+    -v/--version  Print version
+    --help        Print help
+`, {
+  alias: {
+    w: 'width',
+    h: 'height',
+    v: 'version'
+  }
+})
+
+const file = cli.input[0]
+if (!file) {
+  cli.showHelp()
+}
+
+const events = {
+  beforeDownload() {
+    spin.start()
+  },
+  afterDownload() {
+    spin.stop()
+  }
+}
+
+imgcat(file, cli.flags, events)
+  .catch(err => {
+    console.log(err.stack)
+    process.exit(1)
+  })
